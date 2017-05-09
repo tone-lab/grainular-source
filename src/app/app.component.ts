9b5bb667223b019557146b95c33c500e1729b0d1
Darren <darren@darrenmothersele.com>
2017-05-09 14:49:05 +0100

    layers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, NgZone, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, NgZone, OnDestroy, ViewChild} from '@angular/core';
 
 const GrainPlayer = require('tone').GrainPlayer;
 const Transport = require('tone').Transport;
@@ -7,34 +7,52 @@ const Gain = require('tone').Gain;
 const AutoFilter = require('tone').AutoFilter;
 const Freeverb = require('tone').Freeverb;
 const Compressor = require('tone').Compressor;
+const Time = require('tone').Time;
 
 import { map, chunk, sum, first } from 'lodash';
-
+import {Subscription} from "rxjs/Subscription";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/repeat';
+import {animationFrame} from "rxjs/scheduler/animationFrame";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnDestroy {
 
     @ViewChild('wave') wave: ElementRef;
 
     /*
      Cow and Calf sound provided by Felix Blume
      http://www.freesound.org/people/felix.blume/sounds/391026/
+
+     Ahh sound provided by hy96
+     http://www.freesound.org/people/hy96/sounds/48171/
      */
 
     grain;
+    grain2;
+    grain3;
     isLoaded = false;
     gain;
     filter;
+    filter2;
+    filter3;
     spectrum = "M 0 0";
     playerSize = 0;
 
     pt: SVGPoint;
     reverb;
     comp;
+
+    anim: Subscription;
+    playPosition: number;
+
+    ahh;
+    ahhGain;
 
     constructor(private zone: NgZone) {
         this.grain = new GrainPlayer('assets/sounds/cow-and-calf.wav', () => {
@@ -53,11 +71,41 @@ export class AppComponent implements AfterViewInit {
                 this.playerSize = bands.length;
 
                 // start playing:
-                this.grain.start();
+                this.grain.start(0);
+                Transport.start();
+            })
+        });
+        this.grain2 = new GrainPlayer('assets/sounds/cow-and-calf.wav', () => {
+            this.isLoaded = true;
+            this.zone.run(() => {
+                // start playing:
+                this.grain2.start(0);
+                Transport.start();
+            })
+        });
+        this.grain3 = new GrainPlayer('assets/sounds/cow-and-calf.wav', () => {
+            this.isLoaded = true;
+            this.zone.run(() => {
+                // start playing:
+                this.grain3.start(0);
+                Transport.start();
             })
         });
 
-        this.filter = new AutoFilter("4m", 800).start();
+        // this.ahhGain = new Gain();
+        // this.ahh = new GrainPlayer('assets/sounds/ahh.wav', () => {
+        //     this.ahh.start(0);
+        // });
+
+        this.filter = new AutoFilter("6m", 880).start();
+        this.filter.filter.type = "highpass";
+        this.filter.filter.Q.value = 2;
+
+        this.filter2 = new AutoFilter("4m", 440).start();
+        this.filter2.filter.type = "lowpass";
+
+        this.filter3 = new AutoFilter("10m", 1440).start();
+        this.filter3.filter.type = "highpass";
 
         this.gain = new Gain();
         this.gain.connect(Master);
@@ -66,14 +114,20 @@ export class AppComponent implements AfterViewInit {
 
         this.filter.depth.value = 1;
         this.filter.connect(this.reverb);
-        this.grain.connect(this.filter);
+        this.grain.connect(this.filter2);
 
-        this.reverb.roomSize.value = 1;
-        Transport.start();
+        this.filter2.depth.value = 1;
+        this.filter2.connect(this.reverb);
+
+        this.filter3.depth.value = 1;
+        this.filter3.connect(this.reverb);
+
+        this.reverb.roomSize.value = 0.95;
 
         this.grain.grainSize = 0.015;
         this.grain.overlap = 0.25;
-        this.grain.detune = -600;
+        this.grain.loop = true;
+        this.grain.detune = -1200;
         this.grain.playbackRate = 0.2;
         this.grain.drift = 0.05;
 
@@ -81,6 +135,43 @@ export class AppComponent implements AfterViewInit {
         this.comp.connect(this.gain);
         this.reverb.connect(this.comp);
 
+        this.grain2.grainSize = 0.015;
+        this.grain2.overlap = 0.25;
+        this.grain2.loop = true;
+        // this.grain2.detune = -1200;
+        this.grain2.playbackRate = 0.2;
+        this.grain2.drift = 0.05;
+        this.grain2.connect(this.filter);
+
+        this.grain3.grainSize = 0.15;
+        this.grain3.overlap = 0.5;
+        this.grain3.loop = true;
+        this.grain3.detune = 1200;
+        this.grain3.playbackRate = 0.2;
+        this.grain3.drift = 0.75;
+        this.grain3.connect(this.filter3);
+
+
+        // this.ahh.detune = -600;
+        // this.ahh.grainSize = 0.015;
+        // this.ahh.overlap = 0.25;
+        // this.ahh.playbackRate = 0.15;
+        // this.ahh.connect(this.ahhGain);
+        // this.ahhGain.connect(this.reverb);
+        // this.ahhGain.gain.setValueAtTime(0, 0);
+        // this.ahhGain.gain.linearRampToValueAtTime(1, 10);
+
+        this.anim = Observable.of(0, animationFrame)
+            .repeat()
+            .subscribe(() => {
+
+
+
+            });
+    }
+
+    ngOnDestroy() {
+        this.anim.unsubscribe();
     }
 
     ngAfterViewInit()
@@ -94,7 +185,7 @@ export class AppComponent implements AfterViewInit {
     }
 
     skip(e: MouseEvent) {
-        if (this.pt) {
+        if (this.isLoaded && this.pt) {
             this.pt.x = e.clientX;
             this.pt.y = e.clientY;
 
